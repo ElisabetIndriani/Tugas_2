e73d3aaa8b3bc7a8bc7b57715065676768c1a396
ElisabetIndriani <elisabetindri06@gmail.com>
2021-02-25 22:38:00 +0700

    Tugas Kedua

---
+++ Soal_1.py
@@ -0,0 +1,40 @@
+nama=["Farhan" , "Johan"]
+nomor=["08123456789", "08987654321"]
+p=len(nama)
+
+def menu():
+    print("Selamat Datang !")
+    print("---Menu---")
+    print("1. Daftar Kontak")
+    print("2. Tambah Kontak")
+    print("3. Keluar")
+
+menu()
+
+a= int(input("Pilih Menu = "))
+
+if a==1 :
+    print("Daftar Kontak: ")
+    for x in range(p):
+        print ("Nama: ", nama[x])
+        print ("No Telepon: ", nomor[x])
+   
+elif a==2 :
+    x= input("Nama= ")
+    nama.append(x)
+    y= input("No Telepon= ")
+    nomor.append(y)
+    print("Kontak Berhasil ditambahkan")
+
+elif a==3 :
+    print("Program selesai, sampai jumpa!")
+
+else:
+    print("Menu tidak tersedia")
+
+
+
+
+
+
+
